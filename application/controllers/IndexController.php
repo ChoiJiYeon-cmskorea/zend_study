ee0fe34a10cb2aedf10bfff161933096b417f93c
ChoiJiYeon-cmskorea <jiyeonchoi023@gmail.com>
2023-12-19 10:39:53 +0900

    기본 파일 commit

---
+++ application/controllers/IndexController.php
@@ -0,0 +1,14 @@
+<?php
+class IndexController extends Zend_Controller_Action {
+
+    /* (non-PHPdoc)
+     * @see Zend_Controller_Action::init()
+     */
+    public function init() {
+    }
+
+    public function indexAction() {
+        Zend_Debug::dump('test');
+    }
+
+}
